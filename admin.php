52e6d4b3245f5fcf42e9b72d2f58b20b2bf34669
kantinant321 <62507168+kantinant321@users.noreply.github.com>
2020-03-22 20:59:14 +0700

    Update admin.php

--- admin.php
+++ admin.php
@@ -12,10 +12,10 @@
 		document.frmMain.O.src=imgsrc;
 	}
 </script>
-<a href="javascript:A('Image/1.png');"><input type="button" name="1" value="เปิดวิชา"></a> <br>
-<a href="javascript:A('Image/2.png');"><input type="button" name="2" value="ปิดวิชา"></a> <br>
+<a href="javascript:A('img/1.png');"><input type="button" name="1" value="เปิดวิชา"></a> <br>
+<a href="javascript:A('img/2.png');"><input type="button" name="2" value="ปิดวิชา"></a> <br>
 <br><br>
-<img src="../Image/2.png" name="O" width="100" height="100" wid="O"><br>
+<img src="../img/2.png" name="O" width="100" height="100" wid="O"><br>
 </form>
 </body>
 </html>
