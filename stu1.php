755d74c0783a15bc12ddd9f74d39d859f25ef46f
kantinant321 <62507168+kantinant321@users.noreply.github.com>
2020-04-04 11:23:08 +0700

    Add files via upload

--- stu1.php
+++ stu1.php
@@ -1,14 +1,13 @@
-<html>
+ <html>
  <table align= "center" border ="0"><tr><td>
  <form method="post" action="" enctype="multipart/form-data">
- <h1 align="center">--ADMIN--</h1>
+ <h1 align="center">--นักศึกษา--</h1>
  <br>
 
  <a href=".php">-ค้นหาอาจารย์</a><br>
  <a href=".php">-ค้นหานักศึกษา</a><br>
  <a href=".php">-ค้นหารายวิชา</a><br>
- <a href=".php">-ดูข้อมูลรายวิชา</a><br>
- <a href=".php">-เปิด/ปิด รายวิชา</a><br>
-
-</form></td></tr></table>
+ <a href=".php">-ส่งคำร้องขอเปิดรายวิชา</a><br>
+ 
+ </form></td></tr></table>
  </html>
--- teacher1.php
+++ teacher1.php
@@ -1,14 +1,12 @@
 <html>
  <table align= "center" border ="0"><tr><td>
  <form method="post" action="" enctype="multipart/form-data">
- <h1 align="center">--ADMIN--</h1>
+ <h1 align="center">--อาจารย์--</h1>
  <br>
-
- <a href=".php">-ค้นหาอาจารย์</a><br>
+ 
  <a href=".php">-ค้นหานักศึกษา</a><br>
  <a href=".php">-ค้นหารายวิชา</a><br>
  <a href=".php">-ดูข้อมูลรายวิชา</a><br>
- <a href=".php">-เปิด/ปิด รายวิชา</a><br>
-
-</form></td></tr></table>
+  
+ </form></td></tr></table>
  </html>
